1de86fee6dc1997f8ce46c290eb1bf6c65cb5d29
Luis Ocampo <luis.ocampo@admios-sa.com>
2018-08-02 22:50:16 -0400

    First commit

---
+++ server.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname+'/views/partials')
+app.set('view engine', 'hbs')
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) console.log('Unable to append file server.log');
+  })
+  next();
+});
+
+/* Uncomment below code block to enter maintenance status */
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear()
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
+app.get('/',(req, res) => {
+  //res.send('<h1>Hello Expess!</h1>');
+  //res.send({name: 'luis', likes: ['biking','travel']});
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Welcome to Node Web Server'
+  })
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page'
+  })
+});
+
+app.get('/bad', (req, res) => {
+  res.send({errorMessage: 'Not founsd'});
+});
+
+
+app.listen(3000, () => {
+  console.log('Server is up on port 3000.');
+});
